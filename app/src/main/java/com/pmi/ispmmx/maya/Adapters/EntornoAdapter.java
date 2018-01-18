b1e224850fdb6819c2d0c0174f53c50eaabab63e
TheChanec <enrique.nec@gmail.com>
2018-01-18 17:32:07 -0600

    implementar nuevos metodos

--- app/src/main/java/com/pmi/ispmmx/maya/Adapters/EntornoAdapter.java
+++ app/src/main/java/com/pmi/ispmmx/maya/Adapters/EntornoAdapter.java
@@ -6,12 +6,19 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.pmi.ispmmx.maya.Modelos.Entidades.Entorno;
 import com.pmi.ispmmx.maya.R;
+import com.pmi.ispmmx.maya.Utils.CircleTransform;
+import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.NetworkPolicy;
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
+
+import static com.pmi.ispmmx.maya.Utils.Config.HostPreference.URL_FOTOS_ENTORNOS;
 
 public class EntornoAdapter extends RecyclerView.Adapter<EntornoAdapter.ViewHolder> {
 
@@ -50,27 +57,44 @@ public class EntornoAdapter extends RecyclerView.Adapter<EntornoAdapter.ViewHold
         return entornoList.size();
     }
 
+
     public interface OnItemClickListener {
         void OnItemClick(Entorno entorno);
 
+        void OnClickDelete(Entorno entorno);
 
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         private CardView _cvEntorno;
         private TextView _nombre;
-
+        private ImageView image;
+        private ImageView imgDelete;
 
         public ViewHolder(View itemView) {
             super(itemView);
             _cvEntorno = itemView.findViewById(R.id.cv_entorno);
             _nombre = itemView.findViewById(R.id.tv_nombre_entorno);
-
+            image = itemView.findViewById(R.id.img_foto);
+            imgDelete = itemView.findViewById(R.id.img_delete);
         }
 
         public void blind(final Entorno entorno, final OnItemClickListener listener) {
-            _nombre.setText(entorno.getNombre());
+            Picasso.with(context)
+                    .load(URL_FOTOS_ENTORNOS + entorno.getId())
+                    .transform(new CircleTransform())
+                    .error(R.drawable.ic_error_outline_gray)
+                    .networkPolicy(NetworkPolicy.NO_CACHE)
+                    .memoryPolicy(MemoryPolicy.NO_CACHE)
+                    .into(image);
 
+            _nombre.setText(entorno.getNombre());
+            imgDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listener.OnClickDelete(entorno);
+                }
+            });
             _cvEntorno.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
