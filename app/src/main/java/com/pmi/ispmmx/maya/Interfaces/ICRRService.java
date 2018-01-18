a30a95f32820b026746649e692fb8d58fc2b80ce
TheChanec <enrique.nec@gmail.com>
2018-01-18 17:27:04 -0600

    Creacion de la interfaz para CRR

---
+++ app/src/main/java/com/pmi/ispmmx/maya/Interfaces/ICRRService.java
@@ -0,0 +1,21 @@
+package com.pmi.ispmmx.maya.Interfaces;
+
+import com.pmi.ispmmx.maya.Modelos.Entidades.CRR;
+import com.pmi.ispmmx.maya.Modelos.Entidades.VQI;
+import com.pmi.ispmmx.maya.Respuesta.RespuestaServicio;
+import com.pmi.ispmmx.maya.Utils.Config.ApisPreference;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+
+public interface ICRRService {
+    @GET(ApisPreference.ApiCRR)
+    Call<RespuestaServicio<List<CRR>>> getCRRByWorkCenter(@Query("idWorkCenter") int idWorkCenter);
+
+    @GET(ApisPreference.ApiCRR)
+    Call<RespuestaServicio<List<CRR>>> getCRRByWorkCenter(@Query("fecha") String fecha, @Query("idWorkCenter") int idWorkCenter);
+}
