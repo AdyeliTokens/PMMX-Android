0a6a3866a31526534ab25a1cfdc846815e6b4f94
TheChanec <enrique.nec@gmail.com>
2018-01-31 11:00:52 -0600

    cambio de Lookbook a feed

--- app/src/main/java/com/pmi/ispmmx/maya/Adapters/Pages/OperadorPagerAdapter.java
+++ app/src/main/java/com/pmi/ispmmx/maya/Adapters/Pages/OperadorPagerAdapter.java
@@ -5,7 +5,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
 import com.pmi.ispmmx.maya.Fragments.IndicadoresFragment;
-import com.pmi.ispmmx.maya.Fragments.LookBookFragment;
+import com.pmi.ispmmx.maya.Fragments.FeedFragment;
 import com.pmi.ispmmx.maya.Fragments.WorkCenterFragment;
 
 
@@ -14,25 +14,25 @@ public class OperadorPagerAdapter extends FragmentStatePagerAdapter {
     private int numberOfTabs;
     private WorkCenterFragment workCenterFragment;
     private IndicadoresFragment indicadoresFragment;
-    private LookBookFragment lookBookFragment;
+    private FeedFragment feedFragment;
 
-    public OperadorPagerAdapter(FragmentManager fm, int numberOfTabs,LookBookFragment lookBookFragment,IndicadoresFragment indicadoresFragment, WorkCenterFragment workCenterFragment) {
+    public OperadorPagerAdapter(FragmentManager fm, int numberOfTabs, FeedFragment feedFragment, IndicadoresFragment indicadoresFragment, WorkCenterFragment workCenterFragment) {
         super(fm);
         this.numberOfTabs = numberOfTabs;
         this.workCenterFragment = workCenterFragment;
         this.indicadoresFragment=indicadoresFragment;
-        this.lookBookFragment = lookBookFragment;
+        this.feedFragment = feedFragment;
     }
 
     @Override
     public Fragment getItem(int position) {
         switch (position) {
             case 0:
-                return lookBookFragment;
+                return feedFragment;
             case 1:
-                return indicadoresFragment;
+                return workCenterFragment ;
             case 2:
-                return workCenterFragment;
+                return indicadoresFragment;
             default:
                 return null;
         }
