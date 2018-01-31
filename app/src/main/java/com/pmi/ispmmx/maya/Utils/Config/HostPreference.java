3f067c9502f6714c73ee02fae44e8a17d44eefef
TheChanec <enrique.nec@gmail.com>
2018-01-31 10:57:55 -0600

    Ambientes enel host

--- app/src/main/java/com/pmi/ispmmx/maya/Utils/Config/HostPreference.java
+++ app/src/main/java/com/pmi/ispmmx/maya/Utils/Config/HostPreference.java
@@ -2,7 +2,18 @@ package com.pmi.ispmmx.maya.Utils.Config;
 
 public class HostPreference {
 
-    public static final String URL_BASE = "http://serverpmi.tr3sco.net";
+    //Productivo
+    //public static final String URL_BASE = "http://serverpmi.tr3sco.net";
+
+    //Desarrollo
+    public static final String URL_BASE = "http://169.48.177.21:81";
+
+    //Calidad
+    //public static final String URL_BASE = "http://serverpmi.tr3sco.net";
+
+    public static final String URL_FILE = URL_BASE+"/Maya/Download";
+    public static final String URL_INFO_UPDATE = URL_BASE+"/Maya/Info";
+
     public static final String URL_FOTOS_DEFECTOS = URL_BASE+"/Fotos/Defectos/";
     public static final String URL_FOTOS_ORIGENES = URL_BASE+"/Fotos/Origenes/";
     public static final String URL_FOTOS_PERSONAS = URL_BASE+"/Fotos/Personas/";
