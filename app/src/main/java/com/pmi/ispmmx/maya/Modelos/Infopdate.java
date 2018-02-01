abb825d661edf8ab777ea378178e2e74a00177cd
TheChanec <enrique.nec@gmail.com>
2018-02-01 11:07:29 -0600

    Entidad para la info de la update

--- app/src/main/java/com/pmi/ispmmx/maya/Modelos/Infopdate.java
+++ app/src/main/java/com/pmi/ispmmx/maya/Modelos/Infopdate.java
@@ -3,51 +3,21 @@ package com.pmi.ispmmx.maya.Modelos;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-/**
- * Created by chan jacky chan on 29/01/2018.
- */
+
 
 public class Infopdate {
 
 
-    @SerializedName("NewVersion")
+    @SerializedName("versionCode")
     @Expose
-    private Double newVersion;
-
-    @SerializedName("url")
-    @Expose
-    private String URL;
-
-    @SerializedName("releaseNotes")
-    @Expose
-    private String releaseNotes;
-
-    @SerializedName("Fecha")
-    @Expose
-    private String fecha;
+    private int versionCode;
 
 
-    public Double getNewVersion() {
-        return newVersion;
+    public int getVersionCode() {
+        return versionCode;
     }
 
-    public void setNewVersion(Double newVersion) {
-        this.newVersion = newVersion;
-    }
-
-    public String getURL() {
-        return URL;
-    }
-
-    public void setURL(String URL) {
-        this.URL = URL;
-    }
-
-    public String getReleaseNotes() {
-        return releaseNotes;
-    }
-
-    public void setReleaseNotes(String releaseNotes) {
-        this.releaseNotes = releaseNotes;
+    public void setVersionCode(int versionCode) {
+        this.versionCode = versionCode;
     }
 }
